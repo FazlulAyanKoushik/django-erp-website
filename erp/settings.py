c8153cf1d0d79b66b290c93cfd457abf60cae248
FazlulAyanKoushik <ak.fazlulayan117@gmail.com>
2022-08-22 03:34:03 +0600

    deploying

--- erp/settings.py
+++ erp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-rr@i###3qx2tg%$n089zsdr#%&618w$vb9f3ou*21#0!bzinf(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
