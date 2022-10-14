4a3ee92cd311061756f4a40891e1657513670536
FazlulAyanKoushik <ak.fazlulayan117@gmail.com>
2022-10-14 22:04:18 +0600

    ajax search

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
-
+from django.views.decorators.csrf import csrf_exempt
 '''
 expenses
 '''
 
 urlpatterns = [
     path('', views.index, name='expenses'),
-    path('add-expenses/', views.add_expense, name='add-expenses'),
+    path('search-expenses/', csrf_exempt(views.search_expenses), name='search-expenses'),
+    path('add-expense/', views.add_expense, name='add-expense'),
+    path('edit-expense/<int:id>', views.edit_expense, name='edit-expense'),
+    path('delete-expense/<int:id>', views.delete_expense, name='delete-expense'),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,18 +1,57 @@
+import json
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+import expenses
 from userpreference.models import UserPreference
 from .models import Expense, Category
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator
+from django.http import JsonResponse
 
 
 # Create your views here.
 @login_required(login_url='/authentication/login')
+def search_expenses(request):
+    if request.method == 'POST':
+        search_str = json.loads(request.body).get('searchText')
+        print('search_str', search_str)
+        expenses = Expense.objects.filter(
+            amount__istartswith = search_str, user = request.user) | Expense.objects.filter(
+            date__istartswith = search_str, user = request.user) | Expense.objects.filter(
+            description__icontains = search_str, user = request.user) | Expense.objects.filter(
+            category__icontains = search_str, user = request.user)
+        print(f'My Search Expenses: {expenses}')
+        data = expenses.values()
+        return JsonResponse(list(data), safe=False)
+
+            
+@login_required(login_url='/authentication/login')
 def index(request):
-    expenses = Expense.objects.filter(user=request.user)
-    user_preference = UserPreference.objects.get(user=request.user)
+    item_per_page = 4
+    user = User.objects.get(username=request.user)
+    expense_exist = Expense.objects.filter(user=request.user).exists()
+    expenses = None
+    if expense_exist:
+        expenses = Expense.objects.filter(user=request.user).order_by('-date')
+        print(f'My Index Expenses: {expenses}')
+    user_preference_exist  = UserPreference.objects.filter(user=request.user).exists()
+    user_preference = None
+    if user_preference_exist:
+        user_preference = UserPreference.objects.get(user=request.user)
+        
+    # pagination
+    paginator = Paginator(expenses, item_per_page)
+    page_number = request.GET.get('page')
+    expenses_pagination_obj = paginator.get_page(page_number)
+    total_pages = expenses_pagination_obj.paginator.num_pages
+    
     context = {
-        'expenses': expenses,
-        'user_preference': user_preference
+        'user': user,
+        'expenses': expenses_pagination_obj,
+        'user_preference': user_preference,
+        'lastpage': total_pages,
+        'total_page_list': [n+1 for n in range(total_pages)]
     }
     return render(request, 'expenses/index.html', context)
 
@@ -52,3 +91,57 @@ def add_expense(request):
         )
         messages.success(request, 'Expense saved successfully')
         return redirect('expenses')
+
+@login_required(login_url='/authentication/login')
+def edit_expense(request, id):
+    expense = Expense.objects.get(pk=id)
+    categories = Category.objects.all()
+    context = {
+        'form': expense,  
+        'categories': categories,
+    }
+    if request.method == 'GET':
+        return render(request, 'expenses/edit_expense.html', context)
+    
+    if request.method == 'POST':
+        amount = request.POST['amount']
+        description = request.POST['description']
+        category = request.POST['category']
+        date = request.POST['date']
+
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'expenses/edit_expense.html', context)
+        if not category:
+            messages.error(request, 'Category is required')
+            return render(request, 'expenses/edit_expense.html', context)
+        if not date:
+            messages.error(request, 'Date is required')
+            return render(request, 'expenses/edit_expense.html', context)
+        
+        expense.user = request.user
+        expense.amount = amount
+        expense.description = description
+        expense.category = category
+        expense.date = date
+        expense.save()
+        
+        
+        messages.success(request, 'Expense updated successfully')
+        return redirect('expenses')
+    
+def delete_expense(request, id):
+    expense = Expense.objects.get(pk=id)
+    expense.delete()
+    messages.success(request, 'Expense deleted successfully')
+    return redirect('expenses')
+
+
+        
+    
+        
+    
+        
+    
+    
+    
