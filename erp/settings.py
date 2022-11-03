e3de94cfab4e407a759a41ae31c67633c20a9ce2
FazlulAyanKoushik <ak.fazlulayan117@gmail.com>
2022-11-03 14:47:10 +0600

    new heroku update 3

--- erp/settings.py
+++ erp/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = 'django-insecure-rr@i###3qx2tg%$n089zsdr#%&618w$vb9f3ou*21#0!bzinf(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ['erp-flame.herokuapp.com']
+ALLOWED_HOSTS = ['']
+# ALLOWED_HOSTS = ['erp-flame.herokuapp.com']
 
 
 # Application definition
