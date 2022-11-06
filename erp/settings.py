5e91680fce0eec217661b2dfec888c71acf67fb1
Fazlul Ayan Koushik <64150864+FazlulAyanKoushik@users.noreply.github.com>
2022-11-06 19:59:24 +0600

    Update settings.py

--- erp/settings.py
+++ erp/settings.py
@@ -146,14 +146,6 @@ MESSAGE_TAGS = {
 
 
 # email stuff
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = os.environ.get('EMAIL_HOST')
-# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-# EMAIL_USE_TLS = True
-# DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-# EMAIL_PORT = 587
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = 587
